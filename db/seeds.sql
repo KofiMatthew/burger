5b9bb97ff8afc1a47387e95c4a2d204de33c5584
KMZ <mzlatnik@gmail.com>
2019-04-24 20:15:22 -0700

    Essential functionality completed, but not styled

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheese');
+INSERT INTO burgers (name) VALUES ('Double');
+INSERT INTO burgers (name, devoured) VALUES ('Bacon', true);
+INSERT INTO burgers (name, devoured) VALUES ('Mushroom', true);
+INSERT INTO burgers (name, devoured) VALUES ('Chilly', true);
+INSERT INTO burgers (name) VALUES ('Special');
